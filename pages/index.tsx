69ac53a75af60fe6a15af151b8719316a7cef484
tomichans <kaito39smile@gmail.com>
2023-04-26 07:25:04 +0900

    ⚗️ [お試し]コンポーネント内にクリックイベントを設置（useCallback）

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,32 @@
 import Head from 'next/head'
 import Main from '../components/Main'
 import Header from '../components/Header'
+import React, { useCallback, useEffect } from 'react';
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e:React.MouseEvent) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
+  // useEffect(() => {
+  //   document.body.style.backgroundColor = "lightblue"
+
+  //   return () => {
+  //     document.body.style.backgroundColor = ""
+  //   }
+  // }, []);
+
   return (
     <>
       <Head>
         <title>index Page</title>
       </Head>
       <Header />
+      <a href='/about' onClick={handleClick}>ボタン</a>
       <Main page="index" />
     </>
   )
