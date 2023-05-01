651fc05a9e2552179af5cfa91fc756b1b5d71b66
tomichans <kaito39smile@gmail.com>
2023-05-01 13:33:03 +0900

    ⚡️ useCollbackとuseEffectの依存配列を使用した

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,23 @@
 import Head from 'next/head'
 import Main from '../components/Main'
 import Header from '../components/Header'
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e:React.MouseEvent) => {
-    setCount(count => count + 1);
-    setCount(count => count + 1);
-  };
+  const handleClick = useCallback((e:React.MouseEvent) => {
+    if(count < 10) {
+      setCount(count => count + 1);
+    }
+  }, [count]);
 
   useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-
+      document.body.style.backgroundColor = "lightblue";
     return () => {
       document.body.style.backgroundColor = "";
     }
-  }, []);
+  }, [count]);
 
   return (
     <>
