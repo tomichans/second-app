cecdd6e86754ae1d81c8648ff974c8de76e0ae77
tomichans <kaito39smile@gmail.com>
2023-05-07 16:22:57 +0900

    ✨ useStateで文字列と真偽値を管理してイベントを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,10 @@ import React, { useCallback, useEffect, useState } from 'react';
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("aa");
+  const [isShow, setIsShow] = useState(true);
 
-  const handleClick = useCallback((e:React.MouseEvent) => {
+  const handleClick = useCallback(() => {
     if(count < 10) {
       setCount(count => count + 1);
     }
@@ -19,14 +21,30 @@ export default function Home() {
     }
   }, [count]);
 
+  const handleChange = useCallback((e:React.ChangeEvent<HTMLInputElement>) => {
+    if(e.target.value.length > 10) {
+      alert("10文字以内で入力してください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
   return (
     <>
       <Head>
         <title>index Page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>
+        {isShow ? "非表示" : "表示"}
+      </button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
     </>
   )
